4d7d9620bde032789a2e0c73bbb9bcc5838f5c8b
ove1kenobi <ove.odegard@gmail.com>
2019-05-14 16:18:24 +0200

    more code

--- includes/Graph.hpp
+++ includes/Graph.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include<string>
+#include<vector>
+
+class Graph
+{
+    private:
+        struct Edge
+        {
+            size_t target;
+            uint16_t weight;
+        };
+
+        struct Node
+        {
+            std::string name;
+            std::vector<Edge> edges;
+        };
+        
+        bool directed;
+        std::vector<Node> nodes;
+
+    public:
+        Graph();
+        Graph(std::vector<std::string> graphData);
+};
--- source/Graph.cpp
+++ source/Graph.cpp
@@ -1,1 +1,18 @@
 #include "Graph.hpp"
+
+Graph::Graph(std::vector<std::string> graphData)
+{
+    directed = (graphData[0] == "DIRECTED");
+    size_t i = 1;
+    while (graphData[i] != "")
+    {
+        // create node
+        ++i;
+    }
+
+    while (i  < graphData.size())
+    {
+        // create edge
+        ++i;
+    }
+}
