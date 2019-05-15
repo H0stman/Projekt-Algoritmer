777e5918dfaf330e5c783f091f55519aa1678009
ove1kenobi <ove.odegard@gmail.com>
2019-05-15 14:24:39 +0200

    Graph complete

--- includes/Graph.hpp
+++ includes/Graph.hpp
@@ -21,7 +21,8 @@ class Graph
         bool directed;
         std::vector<Node> nodes;
 
+        void CreateEdge(size_t from, size_t to, uint16_t cost);
+
     public:
-        Graph();
-        Graph(bool isDirected, std::vector<std::string> nodeNames, std::vector<std::string> edgeData);
+        Graph(bool isDirected, std::vector<std::string> nodeNames, std::vector<std::vector<std::string>> edgeData);
 };
--- source/Graph.cpp
+++ source/Graph.cpp
@@ -1,18 +1,38 @@
 #include "Graph.hpp"
 
-Graph::Graph(bool isDirected, std::vector<std::string> nodeNames, std::vector<std::string> edgeData)
+Graph::Graph(bool isDirected, std::vector<std::string> nodeNames, std::vector<std::vector<std::string>> edgeData)
 {
-    directed = (graphData[0] == "DIRECTED");
-    size_t i = 1;
-    while (graphData[i] != "")
+    enum Edg {FROM, TO, WEIGHT};
+    
+    directed = isDirected;
+
+    // Create the nodes
+    for (size_t i = 0; i < nodeNames.size(); i++)
     {
-        // create node
-        ++i;
+        Node newNode;
+        newNode.name = nodeNames[i];
+        nodes.push_back(newNode);
     }
 
-    while (i  < graphData.size())
+    // Create edges between nodes
+    for (size_t i = 0; i < edgeData.size(); i++)
     {
-        // create edge
-        ++i;
+        size_t fromNode = 0;
+        size_t toNode = 0;
+        // Find the nodes the edge connects
+        while (nodes[fromNode].name != edgeData[i][Edg::FROM]) ++fromNode;
+        while (nodes[toNode].name != edgeData[i][Edg::TO]) ++toNode;
+        // Create edge and save in start node
+        CreateEdge(fromNode, toNode, stoull(edgeData[i][Edg::WEIGHT]));
+        // If undirected duplicate edge in opposite direction
+        if (!directed) CreateEdge(toNode, fromNode, stoull(edgeData[i][Edg::WEIGHT]));
     }
+}
+
+void Graph::CreateEdge(size_t from, size_t to, uint16_t cost)
+{
+        Edge newEdge;
+        newEdge.target = to;
+        newEdge.weight = cost;
+        nodes[from].edges.push_back(newEdge);
 }
