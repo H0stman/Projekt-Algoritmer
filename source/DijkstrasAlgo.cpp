614883b54ff0ab93468c577a07d240c69b1a847c
ove1kenobi <ove.odegard@gmail.com>
2019-05-21 19:21:40 +0200

    Finished Dijkstras first version

--- source/DijkstrasAlgo.cpp
+++ source/DijkstrasAlgo.cpp
@@ -3,19 +3,22 @@
 
 struct Vertex
 {
-    const uint16_t INFINITY = UINT16_MAX;
+    static const uint16_t INFINITY = UINT16_MAX;
     std::string name;
     std::string previous;
     uint16_t cost;
 
     Vertex();
-    Vertex(std::string input);
+    Vertex(std::string nodeName,uint16_t sumCost);
+    Vertex(std::string nodeName, std::string previousNode, uint16_t sumCost);
     bool operator< (const Vertex &&other);
 };
 
 Vertex::Vertex() : name(""), previous(""), cost(Vertex::INFINITY) {}
 
-Vertex::Vertex(std::string input) : name(input), previous(""), cost(Vertex::INFINITY) {}
+Vertex::Vertex(std::string nodeName, uint16_t sumCost) : name(nodeName), previous(""), cost(sumCost) {}
+
+Vertex::Vertex(std::string nodeName, std::string previousNode, uint16_t sumCost) : name(nodeName), previous(previousNode), cost(sumCost) {}
 
 bool Vertex::operator< (const Vertex &&other)
 {
@@ -25,15 +28,56 @@ bool Vertex::operator< (const Vertex &&other)
 
 std::vector<std::string>& DijkstrasAlgo(const Graph::Graph& network, const std::string& initialNode, const std::string& targetNode)
 {
+    std::vector<Vertex> unvisited;
+    std::vector<Vertex> visited;
+    auto current = unvisited.begin();
+    for (auto &&node : network.getAllNodes())
+    {
+        uint16_t cost = Vertex::INFINITY;
+        if (node == initialNode)
+        {
+            cost = 0;
+            current = unvisited.end();
+        }
+        unvisited.push_back(Vertex(node, cost));
+    }
 
-    std::vector<std::string> unvisited = network.getAllNodes();
-    size_t current = unvisited.begin()->find(initialNode);
     PriorityQueue<Vertex> nextVertex;
 
-    std::vector<std::string> neighbors = network.getNeighbors(unvisited[current]);
-    for (auto i = neighbors.begin(); i != neighbors.end(); i++)
+    do
     {
-        nextVertex.enqueue(Vertex(*i));
-    }
+        std::vector<std::string> neighbors = network.getNeighbors(current->name);
+        for (auto neighbor = neighbors.begin(); neighbor != neighbors.end(); neighbor++)
+        {
+            nextVertex.enqueue(Vertex(*neighbor, current->name, network.getEdgeWeight(current->name, *neighbor) + current->cost));
+        }
+        visited.push_back(*current);
+        unvisited.erase(current);
+        current = unvisited.end();
+        while (current == unvisited.end())
+        {
+            current = unvisited.begin();
+            while ((current->name != nextVertex.peek().name) && (current != unvisited.end())) current++;
+            if (current != unvisited.end())
+            {
+                *current = nextVertex.peek();
+            }
+            nextVertex.dequeue();
+        }
+    } while (current->name != targetNode);
+    std::vector<std::string> shortestPath;
+    shortestPath.push_back(std::to_string(current->cost));
     
+    auto retrace = visited.begin();
+    do
+    {
+        while (retrace->name != current->previous)
+        {
+            ++retrace;
+        }
+        shortestPath.push_back(retrace->name);
+        current = retrace;
+    } while (current->name != initialNode);
+
+    return shortestPath;
 }
